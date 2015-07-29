cd6461bd575aca18cea6611981bf95d610aead79
Michael Choa <mbchoa@gmail.com>
2015-07-29 16:30:26 -0400

    Add method Javadocs

---
+++ src/Deque.java
@@ -0,0 +1,76 @@
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    /**
+     * Initializes Dequue object
+     */
+    public Deque() {
+
+    }
+
+    /**
+     * Returns if queue is empty
+     * @return true if queue is empty, false otherwise
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * Returns the size of the queue
+     * @return integer value of queue size
+     */
+    public int size() {
+
+    }
+
+    /**
+     * Adds item to the beginning of the queue
+     * @param item item added to beginning of queue
+     */
+    public void addFirst(Item item) {
+
+    }
+
+    /**
+     * Add item to the end of the queue
+     * @param item item added to end of queue
+     */
+    public void addLast(Item item) {
+
+    }
+
+    /**
+     * Removes first item from queue
+     * @return item removed from the front
+     */
+    public Item removeFirst() {
+
+    }
+
+    /**
+     * Removes last item from queue
+     * @return item removed from the end
+     */
+    public Item removeLast() {
+
+    }
+
+    /**
+     * Returns iterator over items in order from front to end
+     * @return iterator object over items
+     */
+    public Iterator<Item> iterator() {
+
+    }
+
+    /**
+     * Unit testing
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
