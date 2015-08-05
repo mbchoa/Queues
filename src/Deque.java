d74487c40ac440dd9d150f4617682852300a5a53
Michael Choa <mbchoa@gmail.com>
2015-08-05 13:58:31 -0400

    Implement data structure methods

--- src/Deque.java
+++ src/Deque.java
@@ -1,13 +1,24 @@
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
+    private int N;      // size of list
+    private Node first; // first item in list
+    private Node last;  // last item in list
 
+    // helper linked list class
+    private class Node {
+        private Item item;
+        private Node prev;
+        private Node next;
+    }
     /**
      * Initializes Dequue object
      */
     public Deque() {
-
+        first = last = null;
+        N = 0;
     }
 
     /**
@@ -15,7 +26,7 @@ public class Deque<Item> implements Iterable<Item> {
      * @return true if queue is empty, false otherwise
      */
     public boolean isEmpty() {
-
+        return first == null;
     }
 
     /**
@@ -23,7 +34,7 @@ public class Deque<Item> implements Iterable<Item> {
      * @return integer value of queue size
      */
     public int size() {
-
+        return N;
     }
 
     /**
@@ -31,7 +42,18 @@ public class Deque<Item> implements Iterable<Item> {
      * @param item item added to beginning of queue
      */
     public void addFirst(Item item) {
-
+        if(item == null)
+            throw  new NullPointerException();
+        Node newNode = new Node();
+        newNode.item = item;
+        if (isEmpty()) {
+            first = last = newNode;
+        } else {
+            newNode.next = first;
+            first.prev = newNode;
+            first = newNode;
+        }
+        N++;
     }
 
     /**
@@ -39,7 +61,18 @@ public class Deque<Item> implements Iterable<Item> {
      * @param item item added to end of queue
      */
     public void addLast(Item item) {
-
+        if(item == null)
+            throw  new NullPointerException();
+        Node newNode = new Node();
+        newNode.item = item;
+        if (isEmpty()) {
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+            newNode.prev = last;
+            last = newNode;
+        }
+        N++;
     }
 
     /**
@@ -47,7 +80,18 @@ public class Deque<Item> implements Iterable<Item> {
      * @return item removed from the front
      */
     public Item removeFirst() {
-
+        if(isEmpty())
+            throw new NoSuchElementException();
+        Node nodeToRemove = first;
+        if (N == 1) {
+            first = last = null;
+        } else {
+            first = first.next;
+            first.prev = null;
+            nodeToRemove.next = null;
+        }
+        N--;
+        return nodeToRemove.item;
     }
 
     /**
@@ -55,7 +99,18 @@ public class Deque<Item> implements Iterable<Item> {
      * @return item removed from the end
      */
     public Item removeLast() {
-
+        if(isEmpty())
+            throw new NoSuchElementException();
+        Node nodeToRemove = last;
+        if (N == 1) {
+            first = last = null;
+        } else {
+            last = last.prev;
+            last.next = null;
+            nodeToRemove.prev = null;
+        }
+        N--;
+        return nodeToRemove.item;
     }
 
     /**
@@ -63,7 +118,21 @@ public class Deque<Item> implements Iterable<Item> {
      * @return iterator object over items
      */
     public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
 
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
     }
 
     /**
